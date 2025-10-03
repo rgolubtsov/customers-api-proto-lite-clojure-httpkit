d4802bfb63ff6b6510ca43eeea0838fd9b677895
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-10-03 22:40:30 +0300

    Add constants: (1) Common error messages block. (2) Max and min server port numbers allowed and a default one. Introduce helper function that retrieves the port number used to run the http-kit web server, from daemon settings.

--- src/customers/api_lite/helper.clj
+++ src/customers/api_lite/helper.clj
@@ -23,11 +23,36 @@
 (defmacro MSG-SERVER-STARTED [] "Server started on port ")
 (defmacro MSG-SERVER-STOPPED [] "Server stopped")
 
+; Common error messages.
+(defmacro ERR-PORT-VALID-MUST-BE-POSITIVE-INT [] (str
+    "Valid server port must be a positive integer value, in the range "
+    "1024 .. 49151. The default value of 8080 will be used instead."))
+
 (defmacro SETTINGS "The filename of the daemon settings
     (in edn (Extensible Data Notation) format)." [] "settings.conf")
 
+(defmacro MIN-PORT "The minimum port number allowed." [] 1024 )
+(defmacro MAX-PORT "The maximum port number allowed." [] 49151)
+(defmacro DEF-PORT "The default server port number."  [] 8080 )
+
 ; Helper function. Used to get the daemon settings.
 (defn -get-settings [] (edn/read-string (slurp (io/resource (SETTINGS)))))
+
+; Helper function. Retrieves the port number used to run the http-kit
+;                  web server, from daemon settings.
+(defn -get-server-port [settings]
+    (let [server-port (get settings :server.port)]
+
+    (if-not (nil? server-port)
+        (cond
+            (and (>= server-port (MIN-PORT)) (<= server-port (MAX-PORT)))
+                server-port
+            :else
+                (do (l/error (ERR-PORT-VALID-MUST-BE-POSITIVE-INT)) (DEF-PORT))
+        )
+        (do (l/error (ERR-PORT-VALID-MUST-BE-POSITIVE-INT)) (DEF-PORT))
+    ))
+)
 
 ; Helper function. Used to log messages for debugging aims in a free form.
 (defn -dbg [dbg s message]
